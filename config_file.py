ec4deca54b6034dbe64ce09088e63fe08756d00a
TM-Sanjay <tmsanjay.in@gmail.com>
2021-10-03 16:25:01 +0530

    xl config class add to load & save excel config file

--- config_file.py
+++ config_file.py
@@ -7,9 +7,13 @@ def get_config_file_path():
     return wx.StandardPaths.Get().GetUserConfigDir() + "/esp-flasher-gui.json"
 
 
+def get_xl_config_file_path():
+    return wx.StandardPaths.Get().GetUserConfigDir() + "/esp-flasher-gui-excel.json"
+
+
 class FlashConfig:
     def __init__(self):
-        self.baud = 921600
+        self.baud = '115200'
         self.port = None
         self.firmware_path = None
         self.mode = 'dio'
@@ -41,3 +45,25 @@ class FlashConfig:
         with open(file_path, 'w') as f:
             json.dump(date, f)
 
+
+class ExcelConfig:
+    def __init__(self):
+        self.row_id = 2  # first line will be title
+
+    @classmethod
+    def load(cls):
+        file_path = get_xl_config_file_path()
+        conf = cls()
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            conf.row_id = data['row']
+        return conf
+
+    def save(self):
+        file_path = get_xl_config_file_path()
+        date = {
+            'row': self.row_id
+        }
+        with open(file_path, 'w') as f:
+            json.dump(date, f)
